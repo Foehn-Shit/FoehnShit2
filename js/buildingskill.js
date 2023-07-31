720811bea4673efbc73e08d6bf1cab7b1600252d
Foehn-Shit <mzt1214164232@gmail.com>
2023-07-31 23:51:30 +0800

    Add files via upload

--- js/buildingskill.js
+++ js/buildingskill.js
@@ -2,30 +2,21 @@ function bldSk(obj) {
     switch (obj) {
         case "CONTROL":
             return "控制中枢";
-            break;
         case "POWER":
-            return "发电站"
-            break;
+            return "发电站";
         case "MANUFACTURE":
-            return "制造站"
-            break;
+            return "制造站";
         case "TRADING":
-            return "贸易站"
-            break;
+            return "贸易站";
         case "WORKSHOP":
-            return "加工站"
-            break;
+            return "加工站";
         case "TRAINING":
-            return "训练室"
-            break;
+            return "训练室";
         case "DORMITORY":
-            return "宿舍"
-            break;
+            return "宿舍";
         case "HIRE":
-            return "办公室"
-            break;
+            return "办公室";
         case "MEETING":
-            return "会客室"
-            break;
+            return "会客室";
     }
 }
--- js/nationid.js
+++ js/nationid.js
@@ -1,20 +1,17 @@
-var nationEn = ["none", "rhodes", "yan", "lungmen", "egir", "bolivar", "columbia", "higashi",
-    "iberia", "kazimierz", "kjerag", "laterano", "leithanien", "minos", "rim", "sami",
-    "sargon", "siracusa", "ursus", "victoria", "elite", "sweep", "babel", "abyssal", "blacksteel",
-    "glasgow", "karlan", "lgd", "penguin", "pinus", "rhine", "siesta", "sui", "action4", "reserve1",
-    "reserve4", "reserve6", "chiave", "followers", "lee", "rainbow", "student", null];
-
-var nationZh = ["无团队", "罗德岛", "炎", "炎-龙门", "阿戈尔", "玻利瓦尔", "哥伦比亚", "东", "伊比利亚", "卡西米尔",
-    "谢拉格", "拉特兰", "莱塔尼亚", "米诺斯", "雷姆必拓", "萨米", "萨尔贡", "叙拉古", "乌萨斯", "维多利亚",
-    "罗德岛-精英干员", "S.W.E.E.P", "巴别塔", "深海猎人", "黑钢国际", "格拉斯哥帮", "喀兰贸易", "龙门近卫局",
-    "企鹅物流", "红松骑士团", "莱茵生命", "汐斯塔", "炎-岁", "行动组A4", "行动预备组A1", "行动预备组A4",
-    "行动预备组A6", "贾维团伙", "使徒", "鲤氏侦探事务所", "彩虹小队", "乌萨斯学生自治团", ""];
+// 阵营
 function nationIdLen(obj) {
+    let nationEn = [];
+    let nationZh = [];
+    $.getJSON(`zh_CN/gamedata/excel/handbook_team_table.json`, data => {
+        $.each(data, (id, name) => {
+            nationEn.push(name.powerId);
+            nationZh.push(name.powerName)
+        });
+    });
     for (var i = 0; i < nationEn.length; i++) {
         switch (obj) {
             case nationEn[i]:
-                return nationZh[i]
-                break;
+                return nationZh[i];
         }
     }
-};
+}
